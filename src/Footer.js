2e32cccfe95b3ba5774721f8a1992acfd76b7d86
Márcio Monks <marcio.monks@gmail.com>
2021-06-20 14:43:17 -0300

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+   return (
+      <div className="Footer">Márcio Monks - Copyright 2021</div>
+   )
+}
+
+export default Footer;
